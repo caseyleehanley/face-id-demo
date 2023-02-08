959e624dae9c33b8221f15d458ce6e62a2f03b89
Casey Hanley <chanley1@icloud.com>
2023-02-07 19:46:00 -0600

    Initial Commit

---
+++ FaceIDDemo/FaceIDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDDemoApp.swift
+//  FaceIDDemo
+//
+//  Created by Casey Hanley on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
